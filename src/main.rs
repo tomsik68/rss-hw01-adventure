6878fb948b402f7b191a8a078c65ad6aef0f8047
Tomas Jasek <tomsik68@posteo.net>
2020-08-15 12:20:06 +0200

    Create structures to hold game data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::collections::HashMap;
+
+struct Transition {
+    target_scene: usize,
+    action_text: String,
+}
+
+struct Scene {
+    id: usize,
+    text: String,
+    transitions: Vec<Transition>,
+}
+
+struct Story {
+    scenes: HashMap<usize, Scene>,
+    start_scene: usize,
+}
+
 fn main() {
     println!("Hello, world!");
 }
