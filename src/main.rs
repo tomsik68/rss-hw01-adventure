e2ff75089cb9a8574628e758d5d0c696b2e4a7e0
Tomas Jasek <tomsik68@posteo.net>
2020-08-18 11:33:53 +0200

    Add function to parse story from a file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 use std::collections::HashMap;
+use std::env;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
 
 struct Transition {
     target_scene: String,
@@ -15,6 +20,62 @@ struct Story {
     scenes: HashMap<String, Scene>,
 }
 
+
+fn parse_transitions(line: &str) -> Vec<Transition> {
+    line.split(',')
+        .map(|t| {
+            let mut s = t.split(':');
+            Transition {
+                action_text: s.next().unwrap().trim().to_string(),
+                target_scene: s.next().unwrap().trim().to_string(),
+            }
+        })
+        .collect()
+}
+
+fn parse_story(filename: &dyn AsRef<Path>) -> io::Result<Story> {
+    let f = File::open(filename)?;
+    let br = BufReader::new(f);
+
+    let mut scenes = HashMap::new();
+    let mut scene_id = String::new();
+    let mut scene_text = String::new();
+
+    for (i, line) in br.lines().enumerate() {
+        let line = line?;
+        let line = line.trim();
+        match i % 3 {
+            0 => {
+                scene_id = line.to_string();
+            }
+            1 => {
+                scene_text = line.to_string();
+            }
+            2 => {
+                let transitions = parse_transitions(line);
+                scenes.insert(
+                    scene_id.clone(),
+                    Scene {
+                        id: scene_id,
+                        text: scene_text,
+                        transitions,
+                    },
+                );
+
+                scene_id = String::new();
+                scene_text = String::new();
+            }
+            _ => panic!("i % 3 is none of 0,1,2"),
+        }
+    }
+
+    Ok(Story { scenes })
+}
+
 fn main() {
-    println!("Hello, world!");
+    let filename: String = env::args()
+        .nth(1)
+        .expect("Please supply filename of the story");
+
+    let story = parse_story(&filename).expect("unable to parse story");
 }
